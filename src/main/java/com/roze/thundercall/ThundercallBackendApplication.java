f41eebfab28c0f84db483213d1f7f90135eb41bf
firoze-hossain <firoze.hossain@outlook.com>
2025-08-18 04:46:56 +0600

    first commit

---
+++ src/main/java/com/roze/thundercall/ThundercallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roze.thundercall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThundercallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThundercallBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roze/thundercall/ThundercallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roze.thundercall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThundercallBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

277427b14c1eec2e6da4fd6316b387d7c0972852
firoze-hossain <firoze.hossain@outlook.com>
2025-08-20 13:12:03 +0600

    user entity added

---
+++ src/main/java/com/roze/thundercall/entity/User.java
@@ -0,0 +1,44 @@
+package com.roze.thundercall.entity;
+
+import com.roze.thundercall.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Boolean enabled;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/roze/thundercall/enums/Role.java
@@ -0,0 +1,5 @@
+package com.roze.thundercall.enums;
+
+public enum Role {
+    USER, ADMIN
+}
